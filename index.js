ec20a6287d064b36b69356ad3f28103b96c25019
yukkooooo <calm.and.clear.02@gmail.com>
2024-08-10 15:28:57 +0900

    08コミット

---
+++ index.js
@@ -0,0 +1,25 @@
+// const minusOne = (num) =>{
+// return num-1
+// }
+// console.log(minusOne(3))\\
+
+// let foods = "Orange"
+// let address = "山口県"
+// console.log(foods.length);
+// console.log(address.length);
+
+// const getLength = (str) => {
+// return str.length 
+// }
+// console.log(getLength('apple'))
+
+// const orange = { color: 'orange', price: 300 }
+
+// console.log(orange)
+
+console.log(userDetail.age)
+
+
+
+
+
